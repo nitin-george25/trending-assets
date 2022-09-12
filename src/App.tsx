f0f7bcf9eddd372600d5e920572517b582f7626f
Nitin George <nitin.george25@gmail.com>
2022-09-12 18:18:35 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AssetCard from "./components/AssetCard";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <AssetCard />
+    </div>
+  );
+}
---
+++ src/components/AssetCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AssetCard = () => {
+  return <div>Asset Card</div>
+}
+
+export default AssetCard
